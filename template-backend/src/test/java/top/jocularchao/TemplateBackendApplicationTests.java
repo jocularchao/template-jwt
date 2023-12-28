d11b5ca8069902a444fb7f9469525854ef0e9313
jocularchao <jocularchao@gmail.com>
2023-12-28 13:10:18 +0800

    初始项目创建

---
+++ template-backend/src/test/java/top/jocularchao/TemplateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jocularchao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
