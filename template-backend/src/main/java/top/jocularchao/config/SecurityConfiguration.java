178233738db0ea17e2791aeb897ea71a89ca11a8
jocularchao <jocularchao@gmail.com>
2023-12-28 20:35:53 +0800

    spring security基本配置

---
+++ template-backend/src/main/java/top/jocularchao/config/SecurityConfiguration.java
@@ -0,0 +1,99 @@
+package top.jocularchao.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import top.jocularchao.entity.RestBean;
+
+import java.io.IOException;
+
+/**
+ * @author jocularchao
+ * @date 2023-12-28 13:18
+ * @description
+ */
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                //验证请求拦截和放行配置
+                //爆红了背刺了，6.1版本更新这个方法过时了
+                /*.authorizeHttpRequests()
+                .anyRequest().permitAll()*/
+                .authorizeHttpRequests(conf->conf
+                        .requestMatchers("/api/auth/**").permitAll()    //所有验证的请求再用户没有登录的时候要允许通过
+                        .anyRequest().authenticated()   //任何请求都不允许通过
+                )
+                //登录配置
+                .formLogin(conf -> conf
+                        .loginProcessingUrl("/api/auth/login")
+                        .successHandler(this::onAuthenticationSuccess)  //登录成功的处理
+                        .failureHandler(this::onAuthenticationFailure)  //登录失败的处理
+                )
+                //登出配置
+                .logout(conf->conf
+                        .logoutUrl("api/auth/logout")
+                        .logoutSuccessHandler(this::onLogoutSuccess)    //登出成功的处理
+                )
+                //取消CSRF保护
+                .csrf(AbstractHttpConfigurer::disable)
+                //因为我们现在是无状态的前后端分离跟有状态的前后端分离区别：session不需要去维护用户信息，因为用户信息都在JWT里面
+                //所以我们把sessionManagement改成无状态，让security不去处理session
+                .sessionManagement(conf->conf
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //改成无状态
+                )
+                .build();   //构建，返回默认的SecurityFilterChain对象
+    }
+
+    //登录成功后的处理
+    /**
+     *
+     * @param request   请求
+     * @param response  响应
+     * @param authentication    请求成功后的验证信息
+     * @throws IOException  io异常
+     * @throws ServletException servlet异常
+     */
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        //配置编码
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        response.getWriter().write(RestBean.success().asJsonString());
+    }
+
+    //登录失败后的处理
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        //配置编码
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        response.getWriter().write(RestBean.failure(401,exception.getMessage()).asJsonString());
+    }
+
+    //登出成功的处理
+    //虽然登出成功和登陆成功的参数一样，但由于我们在登出的时候会写一个让jwt失效的功能，所以不能写在一起
+    public void onLogoutSuccess(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Authentication authentication) throws IOException, ServletException {
+
+    }
+
+}
---
+++ template-backend/src/main/java/top/jocularchao/entity/RestBean.java
@@ -0,0 +1,40 @@
+package top.jocularchao.entity;
+
+/**
+ * @author jocularchao
+ * @date 2023-12-28 18:38
+ * @description 装一些用于当前响应的基本信息
+ */
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+/**
+ *
+ * @param code  状态码
+ * @param data  数据
+ * @param message   异常信息
+ * @param <T>   具体的数据的类型不确定
+ */
+public record RestBean<T>(int code, T data,String message) {
+
+    //请求成功
+    public static <T> RestBean<T> success(T data){
+        return new RestBean<>(200,data,"请求成功");
+    }
+    //默认的请求成功
+    public static <T> RestBean<T> success(){
+        return success(null);
+    }
+    public String asJsonString(){
+        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);   //WriteNull 不加上的话，前端的lang就没有了
+    }
+
+
+    //请求失败
+    public static <T> RestBean<T> failure(int code, String message){
+        return new RestBean<>(code,null,message);
+    }
+
+}
