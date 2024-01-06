670f939c6acbd90d2d75be38a0a66ddf8ca4e319
jocularchao <jocularchao@gmail.com>
2024-01-06 19:22:28 +0800

    跨域问题

---
+++ template-backend/src/main/java/top/jocularchao/filter/CorsFilter.java
@@ -0,0 +1,13 @@
+package top.jocularchao.filter;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jocularchao
+ * @date 2024-01-06 19:21
+ * @description 跨域问题
+ */
+@Component
+public class CorsFilter {
+
+}
