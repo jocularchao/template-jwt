bc13647b6df54956e5af752e67c066aff1ab3189
jocularchao <jocularchao@gmail.com>
2024-01-13 22:18:10 +0800

    解决跨域问题

--- template-backend/src/main/java/top/jocularchao/filter/CorsFilter.java
+++ template-backend/src/main/java/top/jocularchao/filter/CorsFilter.java
@@ -1,6 +1,15 @@
 package top.jocularchao.filter;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import top.jocularchao.utils.Const;
+
+import java.io.IOException;
 
 /**
  * @author jocularchao
@@ -8,6 +17,29 @@ import org.springframework.stereotype.Component;
  * @description 跨域问题
  */
 @Component
-public class CorsFilter {
+//spring security的过滤器链 它的优先级很高 默认-100，而跨域的过滤器 包括限流等功能 是要在它之前的
+@Order(Const.ORDER_CORS)
+public class CorsFilter extends HttpFilter {
 
+    @Override
+    protected void doFilter(HttpServletRequest request,
+                            HttpServletResponse response,
+                            FilterChain chain) throws IOException, ServletException {
+        this.addCorsHeader(request,response);
+        //正常放行
+        chain.doFilter(request,response);
+    }
+
+    //在响应头添加跨域信息
+    private void addCorsHeader(HttpServletRequest request,
+                               HttpServletResponse response){
+
+        //允许哪些地址跨域访问        这个Origin是网络请求中前端的地址在浏览器的network中可以查到
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+
+        //添加允许的一些方法
+        response.addHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+        //
+        response.addHeader("Access-Control-Allow-Headers","Authorization, Content-Type");
+    }
 }
--- template-backend/src/main/java/top/jocularchao/utils/Const.java
+++ template-backend/src/main/java/top/jocularchao/utils/Const.java
@@ -6,7 +6,9 @@ package top.jocularchao.utils;
  * @description 存储经常使用的一些属性
  */
 public final class Const {
-    public final static String JWT_BLACK_LIST = "jwt:blacklist:";
+    public final static String JWT_BLACK_LIST = "jwt:blacklist:";   //jwt
+
+    public final static int ORDER_CORS = -102;   //跨域优先级 -102
 
 
 }
